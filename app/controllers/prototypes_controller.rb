96c146806a2baef616e4bb393b2bdd14a0806385
y-naga0926 <75489306+y-naga0926@users.noreply.github.com>
2021-03-22 14:13:16 +0900

    デブロイ後のエラー解決

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,8 @@
 class PrototypesController < ApplicationController
+  before_action :set_prototype, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+  
   def index
     @prototypes = Prototype.includes(:user)
   end
@@ -19,17 +21,18 @@ class PrototypesController < ApplicationController
   end
 
   def show
-    @prototype = Prototype.find(params[:id])
     @comment = Comment.new
     @comments = @prototype.comments
+    @user = User.new
+    @user = @prototype.user
   end
 
   def edit
-    @prototype = Prototype.find(params[:id])
+    @user = User.new
+    @user = @prototype.user
   end
 
   def update
-    @prototype = Prototype.find(params[:id])
     if @prototype.update(prototype_params)
       redirect_to prototype_path(@prototype)
     else
@@ -38,7 +41,8 @@ class PrototypesController < ApplicationController
   end
 
   def destroy
-    @prototype = Prototype.find(params[:id])
+    @user = User.new
+    @user = @prototype.user
     if @prototype.destroy
       redirect_to root_path
     else
@@ -50,6 +54,10 @@ class PrototypesController < ApplicationController
 
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
   end
 
   def contributor_confirmation
